f1e0bd436cf72c34efad33b34865d7c375178053
utsavkejriwal-lti <utsav.kejriwal@lntinfotech.com>
2020-11-26 11:28:32 +0530

    Error rect

--- DemoLibrary.Tests/CalculatorTests.cs
+++ DemoLibrary.Tests/CalculatorTests.cs
@@ -11,7 +11,7 @@ namespace DemoLibrary.Tests
     public class CalculatorTests
     {
         [Theory]
-        [InlineData(4,3,8)]
+        [InlineData(4,3,7)]
         [InlineData(21, 5.25, 26.25)]
         [InlineData(double.MaxValue, 5, double.MaxValue)]
         public void Add_SimpleValuesShouldCalculate(double x, double y, double expected)
